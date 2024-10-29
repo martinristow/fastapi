5b02d4846d73b7e8a0405fd87be066766a0afefc
martinristow <martinristov084@gmail.com>
2024-10-30 00:08:48 +0100

    Add dynamic post ID generation and individual post retrieval
    - Introduce `my_posts` list to store post data
    - Add random ID generation for new posts in `/posts` endpoint
    - Implement `/posts/{id}` endpoint to retrieve individual posts by ID
    - Refactor `/posts` GET request to return all stored posts

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
 app = FastAPI()
 
 
@@ -14,6 +15,16 @@ class Post(BaseModel):
     rating: Optional[int] = None
 
 
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
+            {"title": "favorite foods", "content": "I like pizza", "id": 2}]
+
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
+
 @app.get("/")
 def root():
     return {"message": "welcome to my api!"}
@@ -21,14 +32,24 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your posts"}
+    return {"data": my_posts}
 
 
 
-@app.post('/createposts')
+@app.post('/posts')
 def create_posts(post: Post):
     # print(new_post.rating)
     # print(new_post)
-    print(post.dict())
-    return {"data": post}
+    # print(post.dict())
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
 # title str, content str
+
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    # print(type(id))
+    post = find_post(int(id))
+    return {"post_detail": post}
