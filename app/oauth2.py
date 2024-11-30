0c2e36fade9a622abd371b89b44f5fe1eec7a089
martinristow <martinristov084@gmail.com>
2024-11-30 19:22:27 +0100

    Add JWT-based token creation and verification
    - Created `oauth2.py` to handle JWT token creation and validation.
    - Implemented `create_access_token` function to generate access tokens with expiration.
    - Added `verify_access_token` function to decode and validate JWT tokens.
    - Integrated `get_current_user` function to fetch the authenticated user based on the token.
    - Configured `OAuth2PasswordBearer` to use the `/login` endpoint for token retrieval.

---
+++ app/oauth2.py
@@ -0,0 +1,44 @@
+import jwt
+from jwt.exceptions import InvalidTokenError
+from datetime import datetime, timedelta
+from . import schemas, database, models
+from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
+
+SECRET_KEY = ""
+ALGORITHM = ""
+ACCESS_TOKEN_EXPIRE_MINUTES = 1
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def verify_access_token(token: str, credentials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        id: str = payload.get("user_id")
+        if id is None:
+            raise credentials_exception
+        return schemas.TokenData(id=id)
+    except InvalidTokenError:
+        raise credentials_exception
+
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    token_data = verify_access_token(token, credentials_exception)
+    user = db.query(models.User).filter(token_data.id == models.User.id).first()
+    if user is None:
+        raise credentials_exception
+    return user
