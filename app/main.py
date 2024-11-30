52309decded93a30116a7a6ac94bbc6ed179b38a
martinristow <martinristov084@gmail.com>
2024-11-30 19:23:44 +0100

    Integrate authentication routes and improve database connection
    Added auth.router to include authentication routes in the application.
    Updated the database connection details to connect to the localhost PostgreSQL database with proper credentials.
    Ensured the application integrates routes for post, user, and auth.
    Retained existing code for handling posts and root endpoint functionality.

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from psycopg2.extras import RealDictCursor
 import time
 from app import models
 from app.database import engine
-from .routes import post, user
+from .routes import post, user, auth
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -48,6 +48,7 @@ def find_index_post(id):
 
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(auth.router)
 
 
 @app.get("/")
