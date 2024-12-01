e2af6086f2d618d2d0bf93e2c251fb5774519235
martinristow <martinristov084@gmail.com>
2024-12-01 15:44:12 +0100

    Refactor main.py to streamline database connection and routing setup
    Removed manual psycopg2 database connection setup and replaced it with configurations handled through config.py.
    Simplified code by eliminating hardcoded data (my_posts) and redundant helper functions (find_post, find_index_post).
    Updated FastAPI routing to focus on modular routes (post, user, auth).
    Maintained root endpoint for testing the API's basic functionality.

--- app/main.py
+++ app/main.py
@@ -1,50 +1,12 @@
 from fastapi import FastAPI
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import time
 from app import models
 from app.database import engine
 from .routes import post, user, auth
-
+from .config import Settings
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-# request Get method url: "/"
-
-
-while True:
-
-    try:
-        conn = psycopg2.connect(host='', database='', user='',
-                                password='', cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print("Database connection was succesfull!")
-        break
-    except Exception as error:
-        print("Connecting to database failed")
-        print("Error: ", error)
-        time.sleep(2)
-
-my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
-            {"title": "favorite foods", "content": "I like pizza", "id": 5}]
-
-
-def find_post(id):
-    for p in my_posts:
-        print(p)
-        if p["id"] == id:
-            print(p['id'])
-            return p
-
-
-def find_index_post(id):
-    for i, p in enumerate(my_posts):
-        if p['id'] == id:
-            return i
-
-
 
 app.include_router(post.router)
 app.include_router(user.router)
@@ -54,10 +16,3 @@ app.include_router(auth.router)
 @app.get("/")
 def root():
     return {"message": "welcome to my api!"}
-
-
-# @app.get("/sqlalchemy")
-# def test_posts(db: Session = Depends(get_db)):
-#
-#     posts = db.query(models.Post).all()
-#     return {"data": posts}
