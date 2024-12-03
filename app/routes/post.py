bb8760350ccd9b9aa37016d3ddf86ee13d8c6b5a
martinristow <martinristov084@gmail.com>
2024-12-03 17:20:45 +0100

    Update post.py to include vote counting and enhance query functionality
    Added functionality to count votes using func.count and join with models.Vote.
    Modified get_posts and get_post endpoints to include vote counts in responses.
    Updated response models to use schemas.PostOut for enriched data.
    Refactored query filters for better readability and maintainability.
    Ensured compatibility with existing post creation, deletion, and update operations.

--- app/routes/post.py
+++ app/routes/post.py
@@ -1,6 +1,7 @@
 from typing import List, Optional
 from fastapi import Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
+from sqlalchemy import func
 from app import models, schemas, oauth2
 from app.database import get_db
 
@@ -11,7 +12,8 @@ router = APIRouter(
 )
 
 
-@router.get("/", response_model=List[schemas.Post])
+# @router.get("/", response_model=List[schemas.Post])
+@router.get("/", response_model=List[schemas.PostOut])
 def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user),
               limit: int = 10, skip: int = 0, search: Optional[str] = ""):
     # cursor.execute("""SELECT * FROM posts""")
@@ -23,11 +25,17 @@ def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.
     # posts = db.query(models.Post).limit(limit).offset(skip).all()
 
     # We are searching by a word, in this case we are searching for a title
-    posts = db.query(models.Post).filter(models.Post.title.contains(search)).all()
+    # posts = db.query(models.Post).filter(models.Post.title.contains(search)).all()
 
     # Only if we want to return the posts created by the current user
     # posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id).all()
-    return posts
+
+    results = (db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
+        models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).
+               filter(models.Post.title.contains(search)).limit(limit).offset(skip).all())
+    # print(results)
+
+    return results
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
@@ -58,20 +66,25 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), curren
 # title str, content str
 
 
-@router.get("/{id}", response_model=schemas.Post)
+@router.get("/{id}", response_model=schemas.PostOut)
 def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     # print(type(id))
     # cursor.execute("""SELECT * FROM posts WHERE id = %s """, (str(id),))
     # post = cursor.fetchone()
 
-    post = db.query(models.Post).filter(id == models.Post.id).first()
+    # post = db.query(models.Post).filter(id == models.Post.id).first()
+
+    new_post = (db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
+        models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).
+                filter(id == models.Post.id).first())
+
     # print(post)
-    if not post:
+    if not new_post:
         # response.status_code = status.HTTP_404_NOT_FOUND
         # return {'message': f'post with id: {id} was not found!'}
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f'post with id: {id} was not found!')
-    return post
+    return new_post
 
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
