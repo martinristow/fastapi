819e81ed302d9066d3b2feb84b4ed00683d65bb4
martinristow <martinristov084@gmail.com>
2024-12-04 22:42:36 +0100

    Update main.py to integrate CORS middleware and vote routes
    Replaced models.Base.metadata.create_all with Alembic for table generation.
    Added CORSMiddleware to allow Cross-Origin Resource Sharing (CORS).
    Configured origins to allow all origins (*).
    Included vote.router in the app's routes.
    Updated comments to clarify changes.

--- app/main.py
+++ app/main.py
@@ -1,16 +1,29 @@
 from fastapi import FastAPI
 from app import models
 from app.database import engine
-from .routes import post, user, auth
+from .routes import post, user, auth, vote
 from .config import Settings
+from fastapi.middleware.cors import CORSMiddleware
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine) -> now we don't want this because we used alembic to generate our tables
 
 app = FastAPI()
+
+# origins = ["https://www.google.com"]  # If we want to give permission to only some parties to have access
+origins = ["*"]  # If we want to give permission to all parties to have access
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
+app.include_router(vote.router)
 
 
 @app.get("/")
