2863a042e471ca4fa06b37b6b0edbceaf938b51e
martinristow <martinristov084@gmail.com>
2024-12-08 23:49:18 +0100

    Add unit tests for arithmetic functions in calculations module
    - Parameterized test added for `add` function using `pytest.mark.parametrize`.
    - Individual tests added for:
      - `subtract`
      - `multiply`
      - `divide`
    - Assertions ensure the correctness of the arithmetic operations with sample inputs.
    
    This commit strengthens the reliability of the calculator module by providing test coverage.

---
+++ tests/test_calculations.py
@@ -0,0 +1,25 @@
+import pytest
+
+from app.calculations import add, subtract, multiply, divide
+
+
+@pytest.mark.parametrize("num1, num2, expected", [
+    (3, 2, 5),
+    (7, 1, 8),
+    (12, 4, 16)
+])
+def test_add(num1, num2, expected):
+    print("testing add function")
+    assert add(num1, num2) == expected
+
+
+def test_subtract():
+    assert subtract(9, 4) == 5
+
+
+def test_multiply():
+    assert multiply(4, 3) == 12
+
+
+def test_divide():
+    assert divide(20, 5) == 4
