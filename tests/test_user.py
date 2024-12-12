48cabebeeab79a0910aa6b01b386aa555b99ab85
martinristow <martinristov084@gmail.com>
2024-12-12 19:00:21 +0100

    Refactor `test_user.py` for improved code organization and clarity
    - Imported `client` and `session` from `.database` to centralize test dependencies.
    - Updated test functions to use the `client` fixture for better modularity.
    - Ensured consistent structure and naming for all test cases.
    - No functionality changes; focused on enhancing code readability and maintainability.

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,18 +1,16 @@
-from fastapi.testclient import TestClient
-from app.main import app
 from app import schemas
+from .database import client, session
 
-client = TestClient(app)
 
 
-def test_root():
+def test_root(client):
     response = client.get('/')
     print(response.json().get('message'))
     assert response.status_code == 200
     assert response.json().get('message') == 'welcome to my api!'
 
 
-def test_create_user():
+def test_create_user(client):
     response = client.post('/users', json={"email": "hello123@gmail.com", "password": "password123"})
     # print(response.json())
     new_user = schemas.UserOut(**response.json())
